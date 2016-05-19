15690755fa60b7dde81ded3c2868ce28fbbc931b
kujuman <kujuman@hotmail.com>
2016-05-19 19:14:07 -0400

    1.0
    Initial release. Tested in KSP 1.1.2

---
+++ Source/SaveConfirmationSound/GUI.cs
@@ -0,0 +1,178 @@
+﻿//using System;
+//using System.Linq;
+//using UnityEngine;
+
+//namespace SaveConfirmationSound
+//{
+//    public class GUI
+//    {
+//        private SaveFairingApp app;
+
+//        public Vector2 sitesScrollPosition;
+//        bool goodFirst;
+//        GUIStyle bstyle = new GUIStyle(HighLogic.Skin.button);
+
+//        enum lineType { goodFuel, goodStrut, badFuel, badStrut };
+//        public Part selectedStrut = null;
+
+//        public GUI(SaveFairingApp App, bool ShowGoodStrutsFirst)
+//        {
+//            app = App;
+//            goodFirst = ShowGoodStrutsFirst;
+//        }
+
+//        public void OnGUI()
+//        {
+//            //Debug.Log ("display: " + display.ToString ());
+
+//            if (app.DEBUG) app.Log("GUI.OnGUI()", false);
+
+//            app.window = GUILayout.Window(app.GetInstanceID() + 1, app.window, new UnityEngine.GUI.WindowFunction(draw), "Struts & Fuel Ducts", new GUILayoutOption[0]);
+
+//        }
+//        void showGoodStruts()
+//        {
+//            if (app.DEBUG) app.Log("GUI.showGoodStruts()", false);
+
+//            //  list the good struts
+//            bstyle.normal.textColor = Color.green;
+
+//            //foreach (Part part in app.goodFuelLines)
+//            //{
+//            //    GUILayout.BeginHorizontal();
+//            //    if (GUILayout.Button(part.ToString(), bstyle))
+//            //    {
+//            //        showSelectedPart(lineType.goodFuel, part);
+//            //    }
+//            //    GUILayout.EndHorizontal();
+//            //}
+//            //foreach (Part part in app.goodStruts)
+//            //{
+//            //    GUILayout.BeginHorizontal();
+//            //    if (GUILayout.Button(part.ToString(), bstyle))
+//            //    {
+//            //        showSelectedPart(lineType.goodStrut, part);
+//            //    }
+//            //    GUILayout.EndHorizontal();
+//            //}
+//        }
+//        void showBadStruts()
+//        {
+//            //if (app.DEBUG) app.Log("GUI.showBadStruts()", false);
+//            //// show the bad struts
+//            //bstyle.normal.textColor = Color.red;
+//            //foreach (Part part in app.badFuelLines)
+//            //{
+//            //    GUILayout.BeginHorizontal();
+//            //    if (GUILayout.Button(part.ToString(), bstyle))
+//            //    {
+//            //        showSelectedPart(lineType.badFuel, part);
+//            //    }
+//            //    GUILayout.EndHorizontal();
+//            //}
+//            //foreach (Part part in app.badStruts)
+//            //{
+//            //    GUILayout.BeginHorizontal();
+//            //    if (GUILayout.Button(part.ToString(), bstyle))
+//            //    {
+//            //        showSelectedPart(lineType.badStrut, part);
+//            //    }
+//            //    GUILayout.EndHorizontal();
+//            //}
+//        }
+
+//        void draw(int id)
+//        {
+//            if (app.DEBUG) app.Log("GUI.draw()", false);
+
+//            UnityEngine.GUI.skin = HighLogic.Skin;
+//            GUILayout.Label("Click button to highlight strut for deletion");
+//            sitesScrollPosition = GUILayout.BeginScrollView(sitesScrollPosition);
+//            if (goodFirst)
+//            {
+//                showGoodStruts();
+//                showBadStruts();
+//            }
+//            else
+//            {
+//                showBadStruts();
+//                showGoodStruts();
+//            }
+
+//            GUILayout.EndScrollView();
+//            GUILayout.BeginHorizontal();
+//            if (GUILayout.Button("Hide Window"))
+//            {
+//                app.display = false;
+//            }
+//            if (goodFirst && (app.badFuelLines.Count() + app.badStruts.Count() > 0))
+//            {
+//                if (GUILayout.Button("Show bad struts first"))
+//                {
+//                    selectedStrut = null;
+//                    goodFirst = false;
+//                }
+//            }
+//            else
+//            {
+//                if (app.goodFuelLines.Count() + app.goodStruts.Count() > 0)
+//                {
+//                    if (GUILayout.Button("Show good struts first"))
+//                    {
+//                        selectedStrut = null;
+//                        goodFirst = true;
+//                    }
+//                }
+//            }
+//            if (selectedStrut != null)
+//            {
+//                if (GUILayout.Button("Delete Selected part"))
+//                {
+
+//                    app.Delete(selectedStrut);
+//                    // Repopulate incase a strut with symmetry was deleted
+//                    app.PopulatePartLists();
+//                    selectedStrut = null;
+//                }
+//            }
+//            if (app.badFuelLines.Count() + app.badStruts.Count() > 0)
+//            {
+//                if (GUILayout.Button("Delete all bad parts"))
+//                {
+//                    Part part;
+
+//                    while ((part = app.badFuelLines.FirstOrDefault()) != null)
+//                    {
+//                        try
+//                        {
+//                            app.Delete(part);
+//                        }
+//                        catch (Exception ex)
+//                        {
+//                            app.Log("Deletion of part: " + part.ToString() + "  failed: " + ex, true);
+//                        }
+//                    }
+//                    while ((part = app.badStruts.FirstOrDefault()) != null)
+//                    {
+//                        try
+//                        {
+//                            app.Delete(part);
+//                        }
+//                        catch (Exception ex)
+//                        {
+//                            app.Log("Deletion of part: " + part.ToString() + "  failed: " + ex, true);
+
+//                        }
+//                    }
+
+//                    // Repopulate incase a strut with symmetry was deleted
+//                    app.PopulatePartLists();
+//                    selectedStrut = null;
+//                }
+//            }
+
+//            GUILayout.EndHorizontal();
+//            UnityEngine.GUI.DragWindow();
+//        }
+//    }
+//}
---
+++ Source/SaveConfirmationSound/SaveFairingApp.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace SaveConfirmationSound
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class SaveConfirmationSound : MonoBehaviour
+    {
+        public string settingsURL = "GameData/SaveConfirmationSound/settings.cfg";
+        public bool DEBUG = false;
+        public DateTime LastSaveTime;
+        bool launcherButtonNeedsInitializing = true;
+        int MinimumTimeBetweenSounds = 100;
+        string SoundURL = "SaveConfirmationSound/SoundFile";
+        Stopwatch updateStopwatch;
+
+        void LoadSettings(string sSettingURL)
+        {
+            try
+            {
+                ConfigNode settings = ConfigNode.Load(KSPUtil.ApplicationRootPath + sSettingURL);
+
+                foreach (ConfigNode node in settings.GetNodes("SaveConfirmationSoundSettings"))
+                {
+                    try
+                    {
+                        DEBUG = bool.Parse(node.GetValue("Debug"));
+                        SoundURL = node.GetValue("SoundLocation");
+                        MinimumTimeBetweenSounds = int.Parse(node.GetValue("MinimumTimeBetweenSounds"));
+                    }
+                    catch (Exception)
+                    {
+                        Log("Error loading from config (field)", true);
+                        throw;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Log("Error loading from config (file)", true);
+                throw;
+            }
+        }
+
+        void LateUpdate()
+        {
+            if (launcherButtonNeedsInitializing)
+            {
+                Log("LateUpdate()", false);
+                Audio.initializeAudio();
+
+                GameEvents.onGameStateSaved.Add(OnSave);
+
+                launcherButtonNeedsInitializing = false;
+                updateStopwatch = new Stopwatch();
+                updateStopwatch.Start();
+
+                LoadSettings(settingsURL);
+            }
+        }
+
+        private void OnSave(Game data)
+        {
+            Log("onGameStateSaved", false);
+            Log(updateStopwatch.ElapsedMilliseconds + "ms", false);
+
+            if ((float)updateStopwatch.ElapsedMilliseconds <= MinimumTimeBetweenSounds)
+            {
+                Log("below timer threshold", false);
+                return;
+            }
+            else
+            {
+                updateStopwatch.Reset();
+                updateStopwatch.Start();
+                if (Event.current.Equals(Event.KeyboardEvent(KeyCode.F5.ToString())))
+                {
+                    Log("And Quick Saving", false);
+
+                    if (!MapView.MapIsEnabled)
+                    {
+                        Log("In flight", false);
+                    }
+                    else
+                    {
+                        Log("In map", false);
+                        Audio.markerAudio.transform.SetParent(MapView.MapCamera.transform);
+                    }
+                    Log("Playing Sound " + SoundURL, false);
+                    Audio.markerAudio.PlayOneShot(GameDatabase.Instance.GetAudioClip(SoundURL));
+                    updateStopwatch.Reset();
+                    updateStopwatch.Start();
+                }
+            }
+        }
+
+        public void Log(String message, bool warning)
+        {
+            if (DEBUG && !warning)
+            {
+                UnityEngine.Debug.Log("[SaveConfirmationSound] " + message);
+            }
+            else UnityEngine.Debug.LogWarning("[SaveConfirmationSound] " + message);
+        }
+    }
+
+    public class Audio
+    {
+        private static Audio instance;
+        private Audio() { }
+        public static Audio Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Audio();
+                }
+                return instance;
+            }
+        }
+
+        public static bool isLoaded = false;
+        public static GameObject audioplayer;
+        public static AudioSource markerAudio;
+
+        public static void initializeAudio()
+        {
+            audioplayer = new GameObject();
+            markerAudio = new AudioSource();
+            try
+            {
+                markerAudio = audioplayer.AddComponent<AudioSource>();
+                markerAudio.volume = GameSettings.UI_VOLUME;
+                markerAudio.panStereo = 0;
+                markerAudio.dopplerLevel = 0;
+                markerAudio.bypassEffects = true;
+                markerAudio.loop = true;
+                markerAudio.rolloffMode = AudioRolloffMode.Linear;
+                Audio.markerAudio.transform.SetParent(FlightCamera.fetch.mainCamera.transform);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            isLoaded = true;
+        }
+    }
+}
